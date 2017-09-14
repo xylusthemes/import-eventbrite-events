e91a8ad88270690147d145bce6a95108f11126f4
Dharmesh Patel <dspatel44@gmail.com>
2017-09-14 23:36:57 +0530

    fixed bug "Bulk delete not working for below form"

--- includes/class-import-eventbrite-events-manage-import.php
+++ includes/class-import-eventbrite-events-manage-import.php
@@ -121,7 +121,9 @@ class Import_Eventbrite_Events_Manage_Import {
 			}
 		}
 
-		if ( isset( $_GET['action'] ) && $_GET['action'] == 'delete' && isset($_GET['_wpnonce']) && wp_verify_nonce($_GET['_wpnonce'], 'bulk-iee_scheduled_import') ) {
+		$is_bulk_delete = ( ( isset( $_GET['action'] ) && $_GET['action'] == 'delete' ) || ( isset( $_GET['action2'] ) && $_GET['action2'] == 'delete' ) );
+
+		if ( $is_bulk_delete && isset($_GET['_wpnonce']) && wp_verify_nonce($_GET['_wpnonce'], 'bulk-iee_scheduled_import') ) {
 			$tab = isset($_GET['tab'] ) ? $_GET['tab'] : 'scheduled';
 			$wp_redirect = get_site_url() . urldecode( $_REQUEST['_wp_http_referer'] );
         	$delete_ids = $_REQUEST['iee_scheduled_import'];
@@ -135,7 +137,7 @@ class Import_Eventbrite_Events_Manage_Import {
 			exit;
 		}
 
-		if ( isset( $_GET['action'] ) && $_GET['action'] == 'delete' && isset($_GET['_wpnonce']) && wp_verify_nonce($_GET['_wpnonce'], 'bulk-iee_import_histories') ) {
+		if ( $is_bulk_delete && isset($_GET['_wpnonce']) && wp_verify_nonce($_GET['_wpnonce'], 'bulk-iee_import_histories') ) {
 			$tab = isset($_GET['tab'] ) ? $_GET['tab'] : 'history';
 			$wp_redirect = get_site_url() . urldecode( $_REQUEST['_wp_http_referer'] );
         	$delete_ids = $_REQUEST['import_history'];
