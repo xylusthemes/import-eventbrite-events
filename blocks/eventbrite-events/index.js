1cabbe01a935807d17b6e649bb2dfe1a52f50c86
Dharmesh Patel <dspatel44@gmail.com>
2018-12-06 22:14:12 +0530

    changed date getSettings to _experimentalGetSettings

--- blocks/eventbrite-events/index.js
+++ blocks/eventbrite-events/index.js
@@ -13,7 +13,7 @@ const {
 	ServerSideRender,
 } = wp.components;
 const { InspectorControls } = wp.editor;
-const { dateI18n, getSettings } = wp.date;
+const { dateI18n, __experimentalGetSettings } = wp.date;
 const { createElement } = wp.element;
 
 /**
@@ -92,7 +92,7 @@ registerBlockType( 'iee-block/eventbrite-events', {
 	// Determines what is displayed in the editor
 	edit: function( props ) {
 		const { attributes, isSelected, setAttributes } = props;
-		const settings = getSettings();
+		const settings = __experimentalGetSettings();
 		const dateClassName = attributes.past_events === 'yes' ? 'iee_hidden' : '';
 
 		// To know if the current timezone is a 12 hour time with look for "a" in the time format
@@ -221,7 +221,7 @@ registerBlockType( 'iee-block/eventbrite-events', {
 } );
 
 function eventDateLabel( date, start ) {
-	const settings = getSettings();
+	const settings = __experimentalGetSettings();
 	const defaultLabel = start ? __( 'Select Start Date' ) : __( 'Select End Date' );
 	return date ?
 		dateI18n( settings.formats.datetime, date ) :
