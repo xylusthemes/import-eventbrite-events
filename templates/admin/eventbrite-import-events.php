0f77b4501ee2f06e58291d516d599dbb5470a846
Xylus Dev <support@xylusinfo.com>
2020-04-24 23:13:35 +0530

    Added current user id for none value

--- templates/admin/eventbrite-import-events.php
+++ templates/admin/eventbrite-import-events.php
@@ -77,7 +77,7 @@ global $iee_events;
 								<?php _e('Author','import-eventbrite-events'); ?> :
 							</th>
 							<td>
-								<?php wp_dropdown_users( array( 'show_option_none' => esc_attr__( 'Select Author','import-eventbrite-events'), 'name' => 'event_author' ) ); ?>
+								<?php wp_dropdown_users( array( 'show_option_none' => esc_attr__( 'Select Author','import-eventbrite-events'), 'name' => 'event_author', 'option_none_value' => get_current_user_id() ) ); ?>
 								<span class="iee_small">
 									<?php _e( 'Select event author for imported events. Default event auther is current loggedin user.', 'import-eventbrite-events' ); ?>
 								</span>
