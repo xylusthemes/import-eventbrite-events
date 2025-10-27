da1579ce54e30158366cebfd5b3338b42445166f
Rajat Patel <prajat21@gmail.com>
2025-10-27 16:30:17 +0530

    fixed the pagination condition

--- includes/class-import-eventbrite-events-cpt.php
+++ includes/class-import-eventbrite-events-cpt.php
@@ -551,7 +551,8 @@ class Import_Eventbrite_Events_Cpt {
 	public function eventbrite_events_archive( $atts = array() ) {
 		// [eventbrite_events layout="style2" col='2' posts_per_page='12' category="cat1,cat2" past_events="yes" order="desc" orderby="" start_date="" end_date="" ]
 		$current_date = current_time( 'timestamp' );
-		if ( isset( $atts['ajaxpagi'] ) && $atts['ajaxpagi'] != 'yes' ) {
+		$ajaxpagi     = isset( $atts['ajaxpagi'] ) ? $atts['ajaxpagi'] : '';
+		if ( $ajaxpagi != 'yes' ) {
 			$paged        = ( get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1 );
 			if ( is_front_page() ) {
 				$paged = ( get_query_var( 'page' ) ? get_query_var( 'page' ) : 1 );
